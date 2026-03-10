3b79df1f30c06530d01f1e2a6338463b8305f35b
kurilsang <3050882434@qq.com>
2026-03-10 13:28:49 +0800

    daily: leetcode刷题记录

---
+++ src/main/java/site/kuril/两数之和.java
@@ -0,0 +1,29 @@
+package site.kuril;
+
+import java.util.Vector;
+
+public class 两数之和 {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] result = solution.twoSum(new int[]{3,2,4},6);
+        for(int i=0; i<result.length; i++){
+            System.out.println(result[i]);
+        }
+    }
+}
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for(int j = i+1; j < nums.length; j++) {
+
+                if (nums[i] + nums[j] == target && i!=j) {
+                    result[0] = i;
+                    result[1] = j;
+                }
+            }
+        }
+
+        return result;
+    }
+}
