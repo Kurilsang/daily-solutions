1b05c7992150b17e8f39abd19bdb651be9ba81b7
kurilsang <3050882434@qq.com>
2026-03-11 15:45:05 +0800

    daily: 最长连续序列 这个问题最大的启发点与提示点还是在于去重与不断向前的方法，不断向前可以采用递归或者双指针，去重则可以采用set

---
+++ src/main/java/site/kuril/最长连续序列.java
@@ -0,0 +1,31 @@
+package site.kuril;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class 最长连续序列 {
+    public static void main(String[] args) {
+
+    }
+}
+class Solution3 {
+    public int longestConsecutive(int[] nums) {
+        int count = 0;
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        for(Integer num : set) {
+            int tCount = 1;
+            int start = num;
+            int end = num+1;
+            while(set.contains(end)) {
+                tCount++;
+                end++;
+            }
+            count = Math.max(count, tCount);
+        }
+
+        return count;
+    }
+}
